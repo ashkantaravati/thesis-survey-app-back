cd83caff45f99230123a96c3c6684854158b1983
ashkantaravati <ashkan.taravati@gmail.com>
2021-06-24 15:31:51 +0430

    setup corsheaders

--- thesis_survey/settings.py
+++ thesis_survey/settings.py
@@ -17,16 +17,22 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+CORS_ORIGIN_WHITELIST = [
+    "http://localhost:8080",
 ]
 
 ROOT_URLCONF = "thesis_survey.urls"
