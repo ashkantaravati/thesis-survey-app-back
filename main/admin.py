6375c57ba04869de80e2e81db640506528eed440
ashkantaravati <ashkan.taravati@gmail.com>
2021-07-21 14:00:40 +0430

    add custom inline admins for team participant

--- main/admin.py
+++ main/admin.py
@@ -11,8 +11,56 @@ from .models import (
 
 admin.site.register(Organization)
 admin.site.register(Team)
-admin.site.register(TeamMemberVoiceEvaluationByParticipant)
-admin.site.register(ParticipantTeamMember)
-admin.site.register(GeneralSurveyResponse)
-admin.site.register(OverconfidenceSurveyResponse)
-admin.site.register(TeamCoordinationSurveyResponse)
+# admin.site.register(TeamMemberVoiceEvaluationByParticipant)
+# admin.site.register(ParticipantTeamMember)
+# admin.site.register(GeneralSurveyResponse)
+# admin.site.register(OverconfidenceSurveyResponse)
+# admin.site.register(TeamCoordinationSurveyResponse)
+
+
+class GeneralSurveyResponseInline(admin.StackedInline):
+    model = GeneralSurveyResponse
+
+
+class OverconfidenceSurveyResponseInline(admin.StackedInline):
+    model = OverconfidenceSurveyResponse
+    can_delete = False
+
+
+class TeamCoordinationSurveyResponseInline(admin.StackedInline):
+    model = TeamCoordinationSurveyResponse
+    can_delete = False
+
+
+class VoiceEvaluationsAboutParticipantInline(admin.StackedInline):
+    model = TeamMemberVoiceEvaluationByParticipant
+    can_delete = False
+    fk_name = "evaluated_participant"
+    exclude = ("team",)
+    verbose_name = "Voice behavior evaluation by teammate about this participant"
+    verbose_name_plural = (
+        "Voice behavior evaluations by teammates about this participant"
+    )
+
+
+class VoiceEvaluationsByParticipantInline(admin.StackedInline):
+    model = TeamMemberVoiceEvaluationByParticipant
+    can_delete = False
+    fk_name = "evaluating_participant"
+    exclude = ("team",)
+    verbose_name = "Voice behavior evaluation by this participant about teammate"
+    verbose_name_plural = (
+        "Voice behavior evaluations by this participant about teammates"
+    )
+
+
+@admin.register(ParticipantTeamMember)
+class ParticipantTeamMemberAdmin(admin.ModelAdmin):
+
+    inlines = [
+        GeneralSurveyResponseInline,
+        OverconfidenceSurveyResponseInline,
+        TeamCoordinationSurveyResponseInline,
+        VoiceEvaluationsAboutParticipantInline,
+        VoiceEvaluationsByParticipantInline,
+    ]
