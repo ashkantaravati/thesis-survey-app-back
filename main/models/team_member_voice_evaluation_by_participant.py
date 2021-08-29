b74ca329c6a483731ffc3d010e4ee2f709a3261f
ashkantaravati <ashkan.taravati@gmail.com>
2021-08-29 14:32:08 +0430

    calculate voice behavior score

--- main/models/team_member_voice_evaluation_by_participant.py
+++ main/models/team_member_voice_evaluation_by_participant.py
@@ -1,6 +1,7 @@
 from django.db import models
 from .participant_team_member import ParticipantTeamMember
 from .team import Team
+from django.contrib import admin
 
 
 class TeamMemberVoiceEvaluationByParticipant(models.Model):
@@ -21,6 +22,28 @@ class TeamMemberVoiceEvaluationByParticipant(models.Model):
         related_name="voice_evaluations_about_participant",
     )
     team = models.ForeignKey(to=Team, on_delete=models.DO_NOTHING)
+
+    @property
+    @admin.display(
+        description="Voice Behavior Score",
+    )
+    def score(self):
+        if (
+            self.question_1
+            and self.question_2
+            and self.question_3
+            and self.question_4
+            and self.question_5
+            and self.question_6
+        ):
+            return (
+                self.question_1
+                + self.question_2
+                + self.question_3
+                + self.question_4
+                + self.question_5
+                + self.question_6
+            ) / 6
 
     def __str__(self) -> str:
         return f"نظر {self.evaluating_participant} درباره‌ی رفتار صدای {self.evaluated_participant}"
