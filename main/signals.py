bd72ff5de95dc6574cd88fc9a7f8bfd84ef18d2b
ashkantaravati <ashkan.taravati@gmail.com>
2021-09-03 23:48:15 +0430

    add a signal to send an email to rep after an organization is created

---
+++ main/signals.py
@@ -0,0 +1,27 @@
+from django.db.models.signals import post_save
+from django.core.mail import send_mail
+from django.dispatch import receiver
+from django.template.loader import render_to_string
+from django.utils.html import strip_tags
+from .models import Organization
+
+# method for updating
+@receiver(post_save, sender=Organization, dispatch_uid="send_email_upon_org_reg")
+def send_email_upon_org_reg(sender, instance, created, **kwargs):
+    if created and instance.rep_email:
+        subject = "به پرسشنامه بررسی اثربخشی تیم‌های توسعه نرم‌افزار خوش آمدید."
+        to = instance.rep_email
+        html_message = render_to_string(
+            "main/org_reg_success.html", {"organization": instance}
+        )
+        plain_message = strip_tags(html_message)
+        send_mail(
+            subject,
+            plain_message,
+            recipient_list=[to],
+            html_message=html_message,
+            from_email=None,
+        )
+    # mark as email sent
+    # instance.email_sent = True
+    # instance.save()
