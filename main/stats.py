136b9f7e8104203900b0fe957f2a39f628d782ec
ashkantaravati <ashkan.taravati@gmail.com>
2021-12-30 16:44:25 +0330

    fix ambiguous Response

--- main/stats.py
+++ main/stats.py
@@ -1,4 +1,4 @@
-from rest_framework.response import Response
+from rest_framework.response import Response as APIResponse
 from rest_framework.views import APIView
 from .models import Organization, Response, Team
 
@@ -39,7 +39,7 @@ class StatsView(APIView):
                 for org in orgs
             ],
         }
-        return Response(stats)
+        return APIResponse(stats)
 
 
 class DashboardView(APIView):
