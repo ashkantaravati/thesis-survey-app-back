a0a7a4bda85f7360b50868a0a4014874eb921fc2
ashkantaravati <ashkan.taravati@gmail.com>
2022-01-01 01:58:26 +0330

    fix dashboard according to new changes

--- main/stats.py
+++ main/stats.py
@@ -1,5 +1,7 @@
 from rest_framework.response import Response as APIResponse
 from rest_framework.views import APIView
+
+from main.models import response
 from .models import Organization, Response, Team
 
 TARGET = 70
@@ -54,30 +56,35 @@ class DashboardView(APIView):
                     "rep_name": org.rep_name,
                     "has_participated": org.has_participated,
                     "date_registered": org.created_at,
-                    "number_of_participated_teams": org.number_of_participated_teams,
                     "teams": [
                         {
                             "id": team.id.hashid,
                             "name": team.name,
-                            "size": team.number_of_members,
+                            "size": team.size,
                             "has_participated": team.has_participated,
-                            "participated_count": team.number_of_participated_members,
-                            "members": [
-                                {
-                                    "name": member.name,
-                                    "has_participated": member.has_participated,
-                                    "date_participated": member.updated_at,
-                                }
-                                for member in team.members.all()
+                            "stats": {
+                                "number_of_responses": len(team.responses.all()),
+                                "mean_age": team.mean_age,
+                                "mean_tenure": team.mean_tenure,
+                                "mean_team_history": team.mean_team_history,
+                                "mean_voice_behavior": team.mean_voice_behavior,
+                                "mean_team_coordination": team.mean_team_coordination,
+                                "mean_team_effectiveness": team.mean_team_effectiveness,
+                            },
+                            "respondants": [
+                                {"sex": response.sex, "age": response.age}
+                                for response in team.responses.all()
                             ],
                         }
                         for team in org.teams.all()
                     ],
                 }
-                return Response(org_stats)
+                return APIResponse(org_stats)
             else:
-                return Response({"error": "اطلاعات وارد شده اشتباه است."}, status=403)
-        return Response(status=400)
+                return APIResponse(
+                    {"error": "اطلاعات وارد شده اشتباه است."}, status=403
+                )
+        return APIResponse(status=400)
 
 
 def authenticate_organization_rep(email, token):
